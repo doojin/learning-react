34deb6d7d067f0fff37a4ff14c6316ef857a8a1d
doojin <dmitry.papka@gmail.com>
2019-12-07 13:01:04 +0100

    rendering React app through express

--- packages/server/lib/server.js
+++ packages/server/lib/server.js
@@ -1,10 +1,17 @@
+const path = require('path');
 const express = require('express');
 const articles = require('./data/articles');
 
 const app = express();
 
+app.use(express.static('static/app'));
+
 app.get('/articles', (req, res) => {
 	res.json(articles);
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'static/app/index.html'));
 });
 
 module.exports = {
