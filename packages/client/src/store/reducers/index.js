a99b74682e028ef587e8d51684b30fef6b7cd0bf
doojin <dmitry.papka@gmail.com>
2019-12-07 13:56:04 +0100

    setting up store

---
+++ packages/client/src/store/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+	articles: [
+		{
+			id: 0,
+			title: 'Default Article'
+		}
+	]
+};
+
+function reducer(state = initialState, action) {
+	return state;
+}
+
+export default reducer;
