d9ff77d8a5efc860c63ebeaff52d4f9305d5df4a
doojin <dmitry.papka@gmail.com>
2019-12-27 12:45:29 -0500

    creating ArticlePlaceholder component

---
+++ packages/client/src/components/ArticlePlaceholder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class ArticlePlaceholder extends React.Component {
+  render () {
+    return <div>Your articles are loading</div>
+  }
+}
+
+export default ArticlePlaceholder
