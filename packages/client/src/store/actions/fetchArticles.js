e499bbf353bab989efcc5dc2a938354390349ce6
doojin <dmitry.papka@gmail.com>
2019-12-27 12:47:06 -0500

    adding delay for articles loading

--- packages/client/src/store/actions/fetchArticles.js
+++ packages/client/src/store/actions/fetchArticles.js
@@ -1,26 +1,22 @@
 import axios from 'axios'
+import delay from 'delay'
 
 export const FETCH_ARTICLES_STARTED = 'FETCH_ARTICLES_STARTED'
 export const FETCH_ARTICLES_SUCCESS = 'FETCH_ARTICLES_SUCCESS'
 export const FETCH_ARTICLES_FAILURE = 'FETCH_ARTICLES_FAILURE'
 
-function fetchArticlesStarted () {
-  return { type: FETCH_ARTICLES_STARTED }
-}
+const fetchArticlesStarted = () =>
+  ({ type: FETCH_ARTICLES_STARTED })
 
-function fetchArticlesSuccess (articles) {
-  return {
-    type: FETCH_ARTICLES_SUCCESS,
-    payload: articles
-  }
-}
+const fetchArticlesSuccess = articles => ({
+  type: FETCH_ARTICLES_SUCCESS,
+  payload: articles
+})
 
-function fetchArticlesFailure (error) {
-  return {
-    type: FETCH_ARTICLES_FAILURE,
-    payload: error
-  }
-}
+const fetchArticlesFailure = error => ({
+  type: FETCH_ARTICLES_FAILURE,
+  payload: error
+})
 
 export function fetchArticles () {
   return async dispatch => {
@@ -28,6 +24,7 @@ export function fetchArticles () {
 
     try {
       const articles = await axios.get('/articles')
+      await delay(3000)
       dispatch(fetchArticlesSuccess(articles.data))
     } catch (e) {
       dispatch(fetchArticlesFailure(e))
